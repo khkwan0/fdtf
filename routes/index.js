37050e9a3c138208fdd44eb8ef638f7d8a2f47dc
khkwan0 <khkwan0@gmail.com>
2017-09-22 19:07:17 +0000

    multi lingual

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Freeze Dried Thai Fruits' });
 });
 
+router.get('/de', (req, res, next) => {
+  res.render('index_de.html', { title: 'Freeze Dried Thai Fruits' });
+});
+
+router.get('/cn', (req, res, next) => {
+  res.render('index_cn.html', { title: 'Freeze Dried Thai Fruits' });
+});
+
 module.exports = router;
